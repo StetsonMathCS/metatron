49f5d510aaeae915ffd31613b11c81252ef3f204
Joshua Eckroth <jeckroth@stetson.edu>
2016-06-13 19:52:25 -0400

    Small update to wheel control program, more notes

--- wheel_control/wheel_control.ino
+++ wheel_control/wheel_control.ino
@@ -1,6 +1,11 @@
 #include <Sabertooth.h>
 
 Sabertooth ST(128);
+Sabertooth ST2(129);
+int motor[4];
+int speed[4];
+int i;
+int ok;
 
 void setup() {
   SabertoothTXPinSerial.begin(9600);
@@ -9,17 +14,39 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
-    // first int is motor (1-4)
-    // second int is speed (-127 - 127)
-    int motor = Serial.parseInt();
-    int speed = Serial.parseInt();
-    if(motor >= 1 && motor <= 4 && speed >= -127 && speed <= 127) {
-      ST.motor(motor, speed);
+    // always get four pairs of numbers (each pair is motor, speed)
+
+    for(i = 0; i < 4; i++) {
+      // first int is motor (1-4)
+      // second int is speed (-127 - 127)
+      motor[i] = Serial.parseInt();
+      speed[i] = Serial.parseInt();
+    }
+    // check validity of command
+    ok = 1;
+    for(i = 0; i < 4; i++) {
+      if(motor[i] < 1 || motor[i] > 4 || speed[i] < -127 || speed[i] > 127) {
+        ok = 0;
+        break;
+      }
+    }
+    if(ok == 1) {
+      for(i = 0; i < 4; i++) {
+        if(motor[i] >= 1 && motor[i] <= 2) {
+          ST.motor(motor[i], speed[i]);
+        } else {
+          ST2.motor(motor[i]-2, speed[i]);
+        }
+      }
     } else {
       // bogus command, so kill motors
-      for(int i = 1; i <= 4; i++) {
+      for(int i = 1; i <= 2; i++) {
         ST.motor(i, 0);
+      }
+      for(int i = 1; i <= 2; i++) {
+        ST2.motor(i, 0);
       }
     }
   }
 }
+
