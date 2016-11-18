608e11ec5ca87380ba46675e15f4f9d130b39d5e
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2016-11-18 17:42:39 -0500

    Handle just two wheels

--- wheel_control/wheel_control.ino
+++ wheel_control/wheel_control.ino
@@ -1,9 +1,9 @@
 #include <Sabertooth.h>
 
 Sabertooth ST(128);
-Sabertooth ST2(129);
-int motor[4];
-int speed[4];
+//Sabertooth ST2(129);
+int motor[2];
+int speed[2];
 int i;
 int ok;
 
@@ -14,9 +14,9 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
-    // always get four pairs of numbers (each pair is motor, speed)
+    // always get two pairs of numbers (each pair is motor, speed)
 
-    for(i = 0; i < 4; i++) {
+    for(i = 0; i < 2; i++) {
       // first int is motor (1-4)
       // second int is speed (-127 - 127)
       motor[i] = Serial.parseInt();
@@ -24,28 +24,33 @@ void loop() {
     }
     // check validity of command
     ok = 1;
-    for(i = 0; i < 4; i++) {
-      if(motor[i] < 1 || motor[i] > 4 || speed[i] < -127 || speed[i] > 127) {
+    for(i = 0; i < 2; i++) {
+      if(motor[i] < 1 || motor[i] > 2 || speed[i] < -127 || speed[i] > 127) {
         ok = 0;
         break;
       }
     }
     if(ok == 1) {
-      for(i = 0; i < 4; i++) {
+      for(i = 0; i < 2; i++) {
         if(motor[i] >= 1 && motor[i] <= 2) {
           ST.motor(motor[i], speed[i]);
-        } else {
+        }
+        /*
+         else {
           ST2.motor(motor[i]-2, speed[i]);
         }
+        */
       }
     } else {
       // bogus command, so kill motors
       for(int i = 1; i <= 2; i++) {
         ST.motor(i, 0);
       }
+      /*
       for(int i = 1; i <= 2; i++) {
         ST2.motor(i, 0);
       }
+      */
     }
   }
 }
